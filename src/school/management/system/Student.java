a1daab4837dfb0f6aaa7c2d35f741970db270185
sharif-jobayed <sharif.jobayed@gmail.com>
2024-07-05 04:45:41 +0600

    Set up Student.java

---
+++ src/school/management/system/Student.java
@@ -0,0 +1,24 @@
+package school.management.system;
+
+public class Student {
+    private int id;
+    private String name;
+    private int grade;
+    private int feesPaid;
+    private int feesTotal;
+
+    public Student (int id, String name, int grade) {
+        this.feesPaid = 0;
+        this.feesTotal = 30000;
+        this.id = id;
+        this.name = name;
+        this.grade = grade;
+    }
+
+    public void setGrade (int grade) {
+        this.grade = grade;
+    }
+    public void updateFeesPaid (int feesPaid) {
+        this.feesPaid = feesPaid;
+    }
+}
