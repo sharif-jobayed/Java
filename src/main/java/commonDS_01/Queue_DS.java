c38ecb5dc5d88c498754c761488bdc8461bb4f6d
sharif-jobayed <sharif.jobayed@gmail.com>
2024-11-24 03:06:52 +0600

    Run Queue_DS

---
+++ src/main/java/commonDS_01/Queue_DS.java
@@ -0,0 +1,30 @@
+package commonDS_01;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class Queue_DS {
+    private Queue<Integer> integerQueue;
+
+    public Queue_DS() {
+        this.integerQueue = new LinkedList<>(List.of(1, 2, 654, 6, 1465, 151, 35, 135, 1));
+    }
+
+    /*-------------------------------------------------------------------------------------------------*/
+
+    public static void main(String[] args) {
+        Queue_DS queueDs = new Queue_DS();
+
+        // integerQueue
+        queueDs.integerQueue.offer(110);
+        System.out.println("The items in the integerQueue are: "+queueDs.integerQueue);
+
+        Integer qPeek = queueDs.integerQueue.peek();
+        System.out.println("The item from the integerQueue is: "+qPeek);
+
+        queueDs.integerQueue.poll();
+        System.out.println("The items in the integerQueue are: "+queueDs.integerQueue);
+
+    }
+}
